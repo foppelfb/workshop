40db61665f2c8a125faa7e6ca7befde4da63c69c
Frank Berger <fberger@b-factor.de>
2019-08-04 15:55:58 +0200

    add cache to detailcontroller

--- packages/workshop_blog/Classes/Controller/DetailController.php
+++ packages/workshop_blog/Classes/Controller/DetailController.php
@@ -37,13 +37,34 @@ class DetailController extends ActionController
     
     public function detailAction(Blog $blog)
     {
-        $newcomment = new Comment();
-        
-        $this->view->assignMultiple([
-           'blog'=>$blog,
-           'comments'=>$this->commentRepository->findByBlog($blog),
-           'newcomment'=>$newcomment,
-        ]);
+        $pidOfPlugin = $this->configurationManager->getContentObject()->data['pid'];
+        $uidOfPlugin = $this->configurationManager->getContentObject()->data['uid'];
+    
+        $cacheKey = 'blog-detail-'.$blog->getUid().'-'.$pidOfPlugin.'-'.$uidOfPlugin;
+    
+        $cache = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Cache\CacheManager::class)
+            ->getCache('workshop_blog_cache');
+    
+        if (($data = $cache->get($cacheKey)) === false) {
+            $newcomment = new Comment();
+    
+            $this->view->assignMultiple([
+                'blog' => $blog,
+                'comments' => $this->commentRepository->findByBlog($blog),
+                'newcomment' => $newcomment,
+            ]);
+            // below
+    
+            $data = $this->view->render();
+    
+            $tags = [
+                'pageId_' . $pidOfPlugin,
+                'blogId_' . $blog->getUid()
+            ];
+    
+            $cache->set($cacheKey, $data, $tags, 0);
+        }
+        return $data;
     }
     
     
--- packages/workshop_blog/ext_localconf.php
+++ packages/workshop_blog/ext_localconf.php
@@ -39,3 +39,15 @@ if (!defined('TYPO3_MODE')) {
     ],
     \TYPO3\CMS\Extbase\Utility\ExtensionUtility::PLUGIN_TYPE_PLUGIN
 );
+
+if (!is_array($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['workshop_blog_cache'])) {
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['workshop_blog_cache'] = [
+        'backend'=>\TYPO3\CMS\Core\Cache\Backend\Typo3DatabaseBackend::class,
+        // 'backend'=>\TYPO3\CMS\Core\Cache\Backend\NullBackend::class, //to disable caching
+        'frontend'=>\TYPO3\CMS\Core\Cache\Frontend\VariableFrontend::class,
+        //'groups'=>['pages'],
+        'options'=>[
+            'defaultLifetime'=>0,
+        ]
+    ];
+}
