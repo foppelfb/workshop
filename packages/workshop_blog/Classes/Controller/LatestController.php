93108a8e09c9884f6a44e2b8adb5582650f282ea
Frank Berger <fberger@b-factor.de>
2019-08-04 15:59:07 +0200

    add cache to list and latest controller

--- packages/workshop_blog/Classes/Controller/LatestController.php
+++ packages/workshop_blog/Classes/Controller/LatestController.php
@@ -34,8 +34,25 @@ class LatestController extends ActionController
     
     public function indexAction()
     {
-        $this->view->assignMultiple([
-            'blogs'=>$this->blogRepository->findAll()->getQuery()->setLimit(3)->execute(),
-        ]);
+        $pidOfPlugin = $this->configurationManager->getContentObject()->data['pid'];
+        $uidOfPlugin = $this->configurationManager->getContentObject()->data['uid'];
+        $cacheKey = 'blog-latest-'.$pidOfPlugin.'-'.$uidOfPlugin;
+    
+        $cache = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Cache\CacheManager::class)
+            ->getCache('workshop_blog_cache');
+    
+        if (($data = $cache->get($cacheKey)) === false) {
+            
+            $this->view->assignMultiple([
+                'blogs'=>$this->blogRepository->findAll()->getQuery()->setLimit(3)->execute(),
+            ]);
+            
+            $data = $this->view->render();
+            $tags = ['blog_latest_view'];
+            $tags[]='pageId_'.$pidOfPlugin; // we can diskus about this
+            $cache->set($cacheKey,$data,$tags,0);
+        }
+    
+        return $data;
     }
 }
--- packages/workshop_blog/Classes/Controller/ListController.php
+++ packages/workshop_blog/Classes/Controller/ListController.php
@@ -34,8 +34,36 @@ class ListController extends ActionController
     
     public function indexAction()
     {
-        $this->view->assignMultiple([
-            'blogs'=>$this->blogRepository->findAll(),
-        ]);
+    
+        $pidOfPlugin = $this->configurationManager->getContentObject()->data['pid'];
+        $uidOfPlugin = $this->configurationManager->getContentObject()->data['uid'];
+    
+        $currentPage = 1;
+        if ($this->request->hasArgument('@widget_0')) {
+            $paginateWidget = $this->request->getArgument('@widget_0');
+            if (isset($paginateWidget['currentPage'])) {
+                $currentPage = $paginateWidget['currentPage'];
+            }
+        }
+    
+        $cacheKey = 'blog-list-'.$pidOfPlugin.'-'.$uidOfPlugin.'-'.$currentPage;
+    
+        $cache = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Cache\CacheManager::class)
+            ->getCache('workshop_blog_cache');
+    
+        if (($data = $cache->get($cacheKey)) === false) {
+            
+            $this->view->assignMultiple([
+                'blogs'=>$this->blogRepository->findAll(),
+            ]);
+    
+            $data = $this->view->render();
+            $tags = ['blog_list_view'];
+            $tags[]='pageId_'.$pidOfPlugin; // we can diskus about this
+            $cache->set($cacheKey,$data,$tags,0);
+        }
+    
+        return $data;
+    
     }
 }
