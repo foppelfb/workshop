9bebfe8f299d03ce037bfe30e21dc94e8516c278
Frank Berger <fberger@sudhaus7.de>
2022-04-20 17:53:44 +0200

    upgrade to TYPO3 11.5

--- snippets/addcache_DetailController.php
+++ snippets/addcache_DetailController.php
@@ -3,28 +3,34 @@
 
 //above
 
+use TYPO3\CMS\Core\Cache\CacheManager;
+use TYPO3\CMS\Core\Context\Context;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
 $pidOfPlugin = $this->configurationManager->getContentObject()->data['pid'];
 $uidOfPlugin = $this->configurationManager->getContentObject()->data['uid'];
 
-$cacheKey = 'blog-detail-'.$blog->getUid().'-'.$pidOfPlugin.'-'.$uidOfPlugin;
+$languageid = GeneralUtility::makeInstance( Context::class)->getPropertyFromAspect('language', 'id');
 
-$cache = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Cache\CacheManager::class)
-    ->getCache('workshop_blog_cache');
+$cacheKey = 'blog-detail-'.$blog->getUid().'-'.$pidOfPlugin.'-'.$uidOfPlugin.'-'.$languageid;
+
+$cache = GeneralUtility::makeInstance( CacheManager::class)
+                                               ->getCache('workshop_blog_cache');
 
 if (($data = $cache->get($cacheKey)) === false) {
-    
-    
-    
-    // below
-    
-    $data = $this->view->render();
-    
-    $tags = [
-        'pageId_'.$pidOfPlugin,
-        'blogId_'.$blog->getUid()
-    ];
-    
-    $cache->set($cacheKey,$data,$tags,0);
+
+
+
+	// below
+
+	$data = $this->view->render();
+
+	$tags = [
+		'pageId_'.$pidOfPlugin,
+		'blogId_'.$blog->getUid()
+	];
+
+	$cache->set($cacheKey,$data,$tags,0);
 }
 
-return $data;
+return $this->htmlResponse($data);
