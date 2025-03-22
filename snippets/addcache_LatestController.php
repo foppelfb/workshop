ae8d2e78ae57ea5e67b6c293453f9ac3c27c2bc5
Frank Berger <fberger@sudhaus7.de>
2025-03-22 01:24:34 +0100

    upgrade to typo3 13

--- snippets/addcache_LatestController.php
+++ snippets/addcache_LatestController.php
@@ -1,24 +1,27 @@
 <?php
 
 
-$pidOfPlugin = $this->configurationManager->getContentObject()->data['pid'];
-$uidOfPlugin = $this->configurationManager->getContentObject()->data['uid'];
-$cacheKey = 'blog-latest-'.$pidOfPlugin.'-'.$uidOfPlugin;
+use TYPO3\CMS\Core\Cache\CacheManager;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 
-$cache = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Cache\CacheManager::class)
+$pidOfPlugin = $this->request->getAttribute('currentContentObject')->data['pid'];
+$uidOfPlugin = $this->request->getAttribute('currentContentObject')->data['uid'];
+$cacheKey    = 'blog-latest-'.$pidOfPlugin.'-'.$uidOfPlugin;
+
+$cache = GeneralUtility::makeInstance( CacheManager::class)
     ->getCache('workshop_blog_cache');
 
 if (($data = $cache->get($cacheKey)) === false) {
-    
-    
-    
+
+
+
     // the works
-    
+
     $data = $this->view->render();
-    
-    $tags = ['blog_latest_view'];
-    $tags[]='pageId_'.$pidOfPlugin; // we can diskus about this
+
+	$tags = ['blog_latest_view'];
+	$tags[]='pageId_'.$pidOfPlugin; // we can diskus about this
     $cache->set($cacheKey,$data,$tags,0);
 }
 
-return $data;
+return $this->htmlResponse($data);
