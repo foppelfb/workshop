697654766c5935355b5d57f88567e04a1b2d2af5
Frank Berger <fberger@sudhaus7.de>
2025-03-22 01:05:24 +0100

    upgrade to typo3 13

--- packages/workshop_blog/Configuration/TCA/Overrides/tt_content.php
+++ packages/workshop_blog/Configuration/TCA/Overrides/tt_content.php
@@ -1,20 +1,23 @@
 <?php
 
 
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
+
 call_user_func(function (): void {
-	\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+	ExtensionUtility::registerPlugin(
         'WorkshopBlog',
         'List',
         'Workshop Blog List',
         'EXT:workshop_blog/Resources/Public/Icons/Extension.svg'
     );
-	\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+	ExtensionUtility::registerPlugin(
         'WorkshopBlog',
         'Latest',
         'Workshop Blog Latest',
         'EXT:workshop_blog/Resources/Public/Icons/Extension.svg'
     );
-	\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+	ExtensionUtility::registerPlugin(
         'WorkshopBlog',
         'Detail',
         'Workshop Blog Detail',
@@ -22,8 +25,8 @@ call_user_func(function (): void {
     );
 
     $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['workshopblog_list'] = 'pi_flexform';
-	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility;:addPiFlexFormValue('workshopblog_list', 'FILE:EXT:workshop_blog/Configuration/Flexforms/Flexform.xml');
+	ExtensionManagementUtility::addPiFlexFormValue('workshopblog_list', 'FILE:EXT:workshop_blog/Configuration/Flexforms/Flexform.xml');
 
     $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['workshopblog_latest'] = 'pi_flexform';
-	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue('workshopblog_latest', 'FILE:EXT:workshop_blog/Configuration/Flexforms/Flexform.xml');
+	ExtensionManagementUtility::addPiFlexFormValue('workshopblog_latest', 'FILE:EXT:workshop_blog/Configuration/Flexforms/Flexform.xml');
 });
